1dbeedc07e309998479b46e0769280d600196ac7
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-26 00:13:02 +0530

    correct

--- Quiz Maker/frontend/src/components/CreateQuiz.jsx
+++ Quiz Maker/frontend/src/components/CreateQuiz.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
 const CreateQuiz = () => {
-    const [questions, setQuestions] = useState([{ title: 'rj', options: ['abc', 'cds', '', ''], answer: 1 }, { title: 'r1wqj', options: ['', '', '', ''], answer: 1 }])
+    const [questions, setQuestions] = useState([{ title: 'rj', options: ['abc', 'cds', '', ''], answer: -1 }])
 
-    const handleInputChange = (e, questionIndex, optionIndex = null) => {
+    const handleInputChange = (e, questionIndex, optionIndex = null, selectCorrect = false) => {
         const updatedQuestions = [...questions]
+        if (selectCorrect) {
+            updatedQuestions[questionIndex].answer = optionIndex + 1
+            setQuestions(updatedQuestions)
+            return
+        }
         if (questionIndex !== null && optionIndex !== null) {
             updatedQuestions[questionIndex].options[optionIndex] = e.target.value
             setQuestions(updatedQuestions)
-        }else if (questionIndex !== null && optionIndex === null) {
+        } else if (questionIndex !== null && optionIndex === null) {
             updatedQuestions[questionIndex].title = e.target.value
             setQuestions(updatedQuestions)
         }
     }
 
     const handleAddQuestion = () => {
-        setQuestions([...questions, {title: '', options: ['', '', '', ''], answer: 0}])
+        setQuestions([...questions, { title: '', options: ['', '', '', ''], answer: -1 }])
     }
 
     useEffect(() => {
@@ -26,12 +31,16 @@ const CreateQuiz = () => {
             {questions.map((question, questionIndex) => {
                 return <div key={questionIndex} className='question'>
                     <div className="questionTitle">
+                        <label>{questionIndex + 1}.</label>
                         <input type="text" value={question.title} placeholder='Question' onChange={(e) => handleInputChange(e, questionIndex)} />
                     </div>
                     <div className="options">
                         {question.options.map((option, optionIndex) => {
                             return <div key={optionIndex} className='option'>
-                                <input type="text" value={option} placeholder={'Option ' + (optionIndex + 1)} onChange={(e) => handleInputChange(e, questionIndex, optionIndex)} />
+                                <input type="radio" id={questionIndex + ":" + optionIndex} name={questionIndex} onChange={(e) => handleInputChange(e, questionIndex, optionIndex, true)} />
+                                <label htmlFor={questionIndex + ":" + optionIndex}>
+                                    <input type='text' value={option} placeholder={'Option ' + (optionIndex + 1)} onChange={(e) => handleInputChange(e, questionIndex, optionIndex = optionIndex)} />
+                                </label>
                             </div>
                         })}
                     </div>
