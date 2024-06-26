6d6d53aad5e0f4dc7528bf7c1afc40596ba719ff
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-26 22:35:41 +0530

    localstorage

--- Quiz Maker/frontend/src/components/Quiz.jsx
+++ Quiz Maker/frontend/src/components/Quiz.jsx
@@ -10,17 +10,26 @@ const Quiz = () => {
     const [score, setScore] = useState(0)
     const [totalQuestions, setTotalQuestions] = useState(0)
 
-    async function checkAnswer(){
+    async function checkAnswer() {
         await fetch(`http://localhost:3000/quiz/checkAnswer/${quizId}?question=${questionIndex}&answer=${selectedAnswer}`).then(res => res.json()).then(data => {
-            if (data){
+            if (data.correct) {
+                localStorage.setItem(quizId, score+1)
                 setScore(score + 1)
+            }
+            if (questionIndex + 1 == totalQuestions) {
+                console.log("finish", score)
+            }else{
                 setQuestionIndex(questionIndex + 1)
             }
         })
     }
 
     useEffect(() => {
-        async function getQuestion(){
+        console.log(score)
+    }, [score])
+
+    useEffect(() => {
+        async function getQuestion() {
             document.getElementsByName('option').forEach(radio => radio.checked = false)
             await fetch(`http://localhost:3000/quiz/${quizId}?questionIndex=${questionIndex}`).then(res => res.json()).then(data => {
                 setQuestion(data.question)
@@ -34,6 +43,7 @@ const Quiz = () => {
 
     return (
         <div>
+            <h1>{score}</h1>
             <h2>Q{questionIndex + 1}. {question}</h2>
             {options.map((option, index) => {
                 return (
