57e2d4dd6a1afcc9d8475f3dc32422b445099e3c
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-29 00:25:05 +0530

    take quiz css

--- Quiz Maker/frontend/src/components/Quiz.jsx
+++ Quiz Maker/frontend/src/components/Quiz.jsx
@@ -9,7 +9,7 @@ const Quiz = () => {
     const [questionIndex, setQuestionIndex] = useState(0)
     const [score, setScore] = useState(0)
     const [totalQuestions, setTotalQuestions] = useState(0)
-    const [quizFinished, setQuizFinished] = useState(false)
+    const [quizFinished, setQuizFinished] = useState(true)
 
     async function checkAnswer() {
         await fetch(`http://localhost:3000/quiz/checkAnswer/${quizId}?question=${questionIndex}&answer=${selectedAnswer}`).then(res => res.json()).then(data => {
@@ -44,21 +44,29 @@ const Quiz = () => {
         document.body.appendChild(script);
     }, [])
 
+    const shareButtonShow = () => {
+        
+    }
+
     return (
         <div className='flex flex-col items-center gap-[15px]'>
             {
                 quizFinished ? (
                     <div className='flex flex-row items-center justify-center w-[100vw] h-[100vh] bg-green-400 gap-[10px]'>
-                        <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1 }} loop autoplay direction="1" mode="normal"></lottie-player>
-                        <div className='flex flex-col bg-white border border-black rounded-xl p-[20px]'>
+                        <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 2 }} loop autoplay direction="1" mode="normal"></lottie-player>
+                        <div className='flex flex-col bg-white border border-black rounded-xl p-[20px] gap-[20px] z-[2]'>
                             <span className='font-semibold text-xl'>Congratulations on completing the Quiz!!!</span>
-                            <div className='flex flex-col bg-golden rounded-xl'>
-                                <span>Your Score</span>
-                                <span>{score}/{totalQuestions}</span>
+                            <div className='flex flex-col bg-golden rounded-xl p-[10px] gap-[10px]'>
+                                <span className='font-semibold'>Your Score</span>
+                                <span className='font-bold text-2xl'>{score}/{totalQuestions}</span>
+                                <button className='rounded-2xl bg-blue-500 p-[15px] hover:bg-blue-600 text-white font-semibold mt-[10px]'>Retake Quiz</button>
+                                <button className='rounded-2xl bg-orange-500 p-[15px] hover:bg-orange-600 text-white font-semibold' onClick={shareButtonShow}>Share Your Score</button>
                             </div>
                         </div>
-                        <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1, transform: "scaleX(-1)" }} loop autoplay direction="0" mode="normal"></lottie-player>
-                        <lottie-player src="https://lottie.host/2ef7c797-194b-45d6-9aa8-ea84c43a3ff9/JLM5qM0nEm.json" background="##FFFFFF" speed="0.5" style={{ width: 800, height: 800, position: 'absolute', zIndex: 1 }} loop autoplay direction="1" mode="normal">
+                        <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1, transform: "scaleX(-1)", zIndex: 2 }} loop autoplay direction="0" mode="normal"></lottie-player>
+                        
+                        {/* Firecrackers */}
+                        <lottie-player src="https://lottie.host/2ef7c797-194b-45d6-9aa8-ea84c43a3ff9/JLM5qM0nEm.json" background="##FFFFFF" speed="0.5" style={{ width: 900, height: 950, position: 'absolute', zIndex: 1}} loop autoplay direction="1" mode="normal">
                         </lottie-player>
 
 
@@ -89,7 +97,7 @@ const Quiz = () => {
                                 })}
                             </div>
                             <div className='w-[60vw] mobile:w-[90vw]' dir='rtl'>
-                            <button className='w-[100px] rounded-2xl bg-blue-500 p-[15px] hover:bg-blue-600 text-white font-semibold' onClick={checkAnswer}>Next</button>
+                                <button className='w-[100px] rounded-2xl bg-blue-500 p-[15px] hover:bg-blue-600 text-white font-semibold' onClick={checkAnswer}>Next</button>
                             </div>
                         </>)
             }
