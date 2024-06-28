df02eb39ac7001ec0a3a342c042ce729f4b8bf78
Harsh0707005 <harshmaster07705@gmail.com>
2024-06-28 18:04:29 +0530

    css

--- Quiz Maker/frontend/src/components/CreateQuiz.jsx
+++ Quiz Maker/frontend/src/components/CreateQuiz.jsx
@@ -30,6 +30,8 @@ const CreateQuiz = () => {
             setQuestions(updatedQuestions)
         }
         e.target.style.width = ((e.target.value.length + 3) * 8) + "px"
+        // e.target.style.height = "1px";
+        e.target.style.height = (e.target.scrollHeight) + "px";
     }
 
     const handleAddQuestion = () => {
@@ -42,15 +44,6 @@ const CreateQuiz = () => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ questions })
         }).then(res => res.json()).then(data => setCreatedQuiz(data.url))
-    }
-
-    const handleCopy = async () => {
-        try {
-            await navigator.clipboard.writeText(createdQuiz);
-            console.log('Content copied to clipboard');
-        } catch (err) {
-            console.error('Failed to copy: ', err);
-        }
     }
 
     // useEffect(() => {
@@ -69,12 +62,12 @@ const CreateQuiz = () => {
     })
 
     return (
-        <div className='flex flex-col gap-[20px]'>
+        <div className='flex flex-col gap-[20px] p-[10px]'>
             {createdQuiz ? (
                 <div className='flex flex-row items-center justify-center w-[100vw] h-[100vh] bg-green-400 gap-[10px]'>
                     <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1 }} loop autoplay direction="1" mode="normal"></lottie-player>
                     <ShareSocial title={'Quiz Created Successfully!!! Share on Social Platforms'} style={shareStyle} url={createdQuiz} socialTypes={['facebook', 'whatsapp', 'twitter', 'reddit', 'linkedin', 'telegram', 'line']} />
-                    <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1, transform:"scaleX(-1)" }} loop autoplay direction="0" mode="normal"></lottie-player>
+                    <lottie-player src="https://lottie.host/f61b4eea-5712-42c3-ab5a-79387cfec522/4CFG4nJJoq.json" background="##FFFFFF" speed="0.5" style={{ width: 200, height: 200, zIndex: 1, transform: "scaleX(-1)" }} loop autoplay direction="0" mode="normal"></lottie-player>
                     <lottie-player src="https://lottie.host/2ef7c797-194b-45d6-9aa8-ea84c43a3ff9/JLM5qM0nEm.json" background="##FFFFFF" speed="0.5" style={{ width: 800, height: 800, position: 'absolute', zIndex: 1 }} loop autoplay direction="1" mode="normal">
                     </lottie-player>
 
@@ -95,14 +88,14 @@ const CreateQuiz = () => {
                         <div key={questionIndex} className='question flex flex-col gap-[15px]'>
                             <div className="questionTitle flex flex-col gap-4 items-center justify-center">
                                 <p className='text-2xl font-bold'>Question {questionIndex + 1}</p>
-                                <input className='rounded-md border-gray-400 border outline-none p-[5px]' type="text" value={question.title} placeholder='Question' onChange={(e) => handleInputChange(e, questionIndex)} />
+                                <textarea rows={1} className='resize-none rounded-md border-gray-400 border outline-none p-[5px] min-w-[250px] max-w-[80vw]' type="text" value={question.title} placeholder='Question' onChange={(e) => handleInputChange(e, questionIndex)} />
                             </div>
                             <div className="options flex flex-col justify-center gap-[20px]">
                                 {question.options.map((option, optionIndex) => (
                                     <div key={optionIndex} className='option flex flex-row gap-[10px] items-center rounded-lg p-[10px]'>
                                         <input type="radio" id={questionIndex + ":" + (optionIndex + 1)} name={questionIndex} onChange={(e) => handleInputChange(e, questionIndex, optionIndex, true)} />
                                         <label htmlFor={questionIndex + ":" + optionIndex}>
-                                            <input className='rounded-md border-gray-400 border outline-none p-[5px]' type='text' value={option} placeholder={'Option ' + (optionIndex + 1)} onChange={(e) => handleInputChange(e, questionIndex, optionIndex)} />
+                                            <textarea rows={1} className='resize-none rounded-md border-gray-400 border outline-none p-[5px] min-w-[250px] max-w-[80vw]' type='text' value={option} placeholder={'Option ' + (optionIndex + 1)} onChange={(e) => handleInputChange(e, questionIndex, optionIndex)} />
                                         </label>
                                     </div>
                                 ))}
